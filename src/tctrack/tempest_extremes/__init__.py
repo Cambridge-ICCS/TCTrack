5aca520695f6c8b6d878064c92699e0acddef5c8
jatkinson1000 <jwa34@cam.ac.uk>
2025-05-28 17:13:59 +0100

    First effort at wrapping Tempest Extremes.
    Provides a tempest_extremes module and simple scaffolding.
    Minimal wrapping of the DetectNodes functionality.

---
+++ src/tctrack/tempest_extremes/__init__.py
@@ -0,0 +1,10 @@
+"""Package bindings to the Tempest Extremes tracking code."""
+
+from .tempest_extremes import (
+    DetectNodesParameters,
+    TEContour,
+    TEOutputComand,
+    TETracker,
+)
+
+__all__ = ["DetectNodesParameters", "TEContour", "TEOutputComand", "TETracker"]
