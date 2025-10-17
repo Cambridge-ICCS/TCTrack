ecdfeb16af4a7c2af4e9e4c3cbecb28c9489da1a
Sam Avis <avissam96@gmail.com>
2025-10-17 15:43:14 +0100

    lat_lon_sizes docstring and add to tctrack.utils

--- src/tctrack/utils/__init__.py
+++ src/tctrack/utils/__init__.py
@@ -1,1 +1,7 @@
 """Package providing utility functions used throughout TCTrack."""
+
+from .netcdf import lat_lon_sizes
+
+__all__ = [
+    "lat_lon_sizes",
+]
--- src/tctrack/utils/netcdf.py
+++ src/tctrack/utils/netcdf.py
@@ -4,7 +4,24 @@ from netCDF4 import Dataset
 
 
 def lat_lon_sizes(filename: str, lat: str = "lat", lon: str = "lon") -> tuple[int, int]:
-    """Get the sizes of the longitude and latitude for a netcdf file."""
+    """Get the sizes of the longitude and latitude for a netcdf file.
+
+    Parameters
+    ----------
+    filename : str
+        The path to the netcdf file to read from.
+    lat : str, default="lat"
+        The netcdf variable name of the latitude.
+    lon : str, default="lon"
+        The netcdf variable name of the longitude.
+
+    Returns
+    -------
+    int
+        The size of the latitude dimension.
+    int
+        The size of the longitude dimension.
+    """
     dataset = Dataset(filename, "r")
     lon_size = len(dataset.dimensions[lon])
     lat_size = len(dataset.dimensions[lat])
