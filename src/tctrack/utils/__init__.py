582aabfdd4b770132301217ba6c268129e75ab79
Sam Avis <avissam96@gmail.com>
2026-05-12 13:02:44 +0100

    Added utilities to read metadata from outputs

--- src/tctrack/utils/__init__.py
+++ src/tctrack/utils/__init__.py
@@ -1,5 +1,8 @@
 """Package providing utility functions for the user."""
 
+from .metadata import load_tracker_metadata, read_tracker_metadata
 
 __all__ = [
+    "load_tracker_metadata",
+    "read_tracker_metadata",
 ]
